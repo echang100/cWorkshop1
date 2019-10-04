1dd8e7a3f276bdf11d92e269265bcf0c466b2869
echang100 <echang100@student.mtsac.edu>
2019-10-04 13:37:29 -0700

    First commit

---
+++ Test1/Test1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test1
+//
+//  Created by Eric Chang on 10/4/19.
+//  Copyright © 2019 Eric Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
